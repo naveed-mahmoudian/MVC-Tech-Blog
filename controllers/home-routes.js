54271c388cfd3cce5fa78c0f7bca30fc9766f891
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-11-15 12:24:50 -0800

    Setting up homepage routes and views for posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
 
+const posts = [
+  {
+    id: 1,
+    date_created: "11/15/22",
+    title: "My First Post",
+    text: "This is my first post...WOW!",
+    username: "nav",
+  },
+  {
+    id: 2,
+    date_created: "11/16/22",
+    title: "My Second Post",
+    text: "This is my second post here!",
+    username: "nav",
+  },
+];
+
 router.get("/", async (req, res) => {
-  res.render("home");
+  res.render("home", { posts });
 });
 
 module.exports = router;
